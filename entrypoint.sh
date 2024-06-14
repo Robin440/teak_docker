8ce2c7a3092472d661e9d1517203ea926f3bf4c0
robin <robin.rajan@peerxp.com>
2024-06-14 12:32:27 +0530

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-source /teak_env/bin/activate
-pip install -r requirements.txt
-RUN pip install django gunicorn
+sudo source /teak_env/bin/activate
+sudo pip install -r requirements.txt
+# RUN pip install django gunicorn
 
 
-python3 manage.py makemigrations
+sudo python3 manage.py makemigrations
 # Apply database migrations
-python3 manage.py migrate
+sudo python3 manage.py migrate
 
 # Collect static files (if needed)
 # python3 manage.py collectstatic 
 
 # Start Gunicorn server
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8009
+sudo python manage.py runserver 0.0.0.0:8000
+# exec gunicorn core.wsgi:application --bind 0.0.0.0:8009
